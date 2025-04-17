badd84aaa27e5385c385ea129b10b268317e1c79
charityrymbai <charityrymbai@hotmail.com>
2025-04-17 16:51:28 +0530

    added jwt auth

--- __init__.py
+++ __init__.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from backend.routes.user_routes import user_bp
 from backend.routes.auth_routes import auth_bp
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    jwt.init_app(app)
+
+    load_dotenv()  # Load environment variables from a .env file
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)  # 15 minutes
+    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30) # 30 days
+
     app.register_blueprint(user_bp, url_prefix='/api/v1/user')
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
+    
     return app
 
 
--- services/auth_service.py
+++ services/auth_service.py
@@ -1,2 +1,24 @@
+from flask_jwt_extended import (create_access_token, create_refresh_token)
+from flask import jsonify
+from datetime import timedelta
+
 def login(data): 
-    return { data['username']: data['password'] }
+    username = data.get('username')
+    password = data.get('password')
+
+    # database lookup
+
+    user = {
+        'id': 1,
+        'username': username,
+        'password': password
+    }
+
+    access_token = create_access_token(identity=user['id'])
+    refresh_token = create_refresh_token(identity=user['id'])
+
+    return jsonify({
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    })
+    
