2e27754c5e6ceb564bf85e4c157ad78deedae2b4
Jishnu Duhan <duhanjishnu@gmail.com>
2025-04-20 19:50:22 +0530

    made separeate vector db for lost and found

--- services/ai_services.py
+++ services/ai_services.py
@@ -7,8 +7,12 @@ from langchain_core.documents import Document
 
 load_dotenv()
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
-UPSTASH_VECTOR_REST_URL = os.getenv("UPSTASH_VECTOR_REST_URL")
-UPSTASH_VECTOR_REST_TOKEN = os.getenv("UPSTASH_VECTOR_REST_TOKEN")
+UPSTASH_VECTOR_REST_URL_FOUND = os.getenv("UPSTASH_VECTOR_REST_URL_FOUND")
+UPSTASH_VECTOR_REST_TOKEN_FOUND = os.getenv("UPSTASH_VECTOR_REST_TOKEN_FOUND")
+
+
+UPSTASH_VECTOR_REST_URL_LOST = os.getenv("UPSTASH_VECTOR_REST_URL_LOST")
+UPSTASH_VECTOR_REST_TOKEN_LOST = os.getenv("UPSTASH_VECTOR_REST_TOKEN_LOST")
 
 # create embeddings and insert into vector store
 def embed_service():
@@ -16,6 +20,14 @@ def embed_service():
         data = request.get_json()
         text = data["itemDesc"]
         _id = data["_id"]
+        itemCategory = data["itemCategory"]
+
+        if itemCategory == "lost":
+            UPSTASH_VECTOR_REST_URL = UPSTASH_VECTOR_REST_URL_LOST
+            UPSTASH_VECTOR_REST_TOKEN = UPSTASH_VECTOR_REST_TOKEN_LOST
+        else:
+            UPSTASH_VECTOR_REST_URL = UPSTASH_VECTOR_REST_URL_FOUND
+            UPSTASH_VECTOR_REST_TOKEN = UPSTASH_VECTOR_REST_TOKEN_FOUND
         
         embeddings = MistralAIEmbeddings(
             model="mistral-embed",
@@ -25,7 +37,9 @@ def embed_service():
         documents = [Document(page_content=text, metadata={"_id": _id})]
 
         store = UpstashVectorStore(
-            embedding=embeddings
+            embedding=embeddings,
+            index_url=UPSTASH_VECTOR_REST_URL,
+            index_token=UPSTASH_VECTOR_REST_TOKEN
         )
 
         store.add_documents(documents)
